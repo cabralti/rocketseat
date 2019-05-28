213ca8e2719798f11c88bdeb19a2deffe52d40dd
Gabriel Cabral <cabral.9santos@gmail.com>
2019-05-28 20:34:36 -0300

    Salvando em Storage

--- curso_javacript/todolist/todolist.js
+++ curso_javacript/todolist/todolist.js
@@ -2,10 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodoList() {
 
@@ -35,6 +32,7 @@ function addTodo() {
     inputElement.value = '';
 
     renderTodoList();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -42,6 +40,11 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
     todos.splice(pos, 1);
     renderTodoList();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
 
 renderTodoList();
